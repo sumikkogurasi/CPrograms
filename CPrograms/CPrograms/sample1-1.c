29d242512a4a01c101b19313e7d534c1de2faa56
ebiflychan <sumikkogurasi555@gmail.com>
2023-07-28 12:39:41 +0900

    add sample1-2

--- CPrograms/CPrograms/sample1-1.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-	printf("CŒ¾Œê‚ÌƒvƒƒOƒ‰ƒ€‚ğì‚Á‚ÄŒ©‚æ‚¤I\n");
-
-	return 0;
-}
---
+++ CPrograms/CPrograms/sample1-2.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(void) {
+	printf("CŒ¾Œê‚ÌƒvƒƒOƒ‰ƒ€1\n");
+
+	return 0;
+}
