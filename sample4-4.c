85c9d6fa48ec8a0f9716a6179c5e535bd5ee7e62
ebiflychan <sumikkogurasi555@gmail.com>
2023-07-28 18:45:30 +0900

    add sample4-5

---
+++ sample4-4.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void){
+    printf("%d円の商品を三つ購入した赤くは%d円です。\n", 1000, 1000 * 3);
+
+    return 0;
+}
---
+++ sample4-5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+    printf("%d × %d = %2d\n", 5, 1, 5 * 1);
+    printf("%d × %d = %2d\n", 5, 1, 5 * 2);
+    printf("%d × %d = %2d\n", 5, 1, 5 * 3);
+    printf("%d × %d = %2d\n", 5, 1, 5 * 4);
+    printf("%d × %d = %2d\n", 5, 1, 5 * 5);
+    printf("%d × %d = %2d\n", 5, 1, 5 * 6);
+    printf("%d × %d = %2d\n", 5, 1, 5 * 7);
+    printf("%d × %d = %2d\n", 5, 1, 5 * 8);
+    printf("%d × %d = %2d\n", 5, 1, 5 * 9);
+    
+    return 0;
+}
